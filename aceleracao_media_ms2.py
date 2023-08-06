f8d6b339923ce3d70e47950af4b93232f9fc04cf
edilson gomes de lima <edskateicevida@gmail.com>
2023-08-06 17:42:21 -0300

    Aceleração

---
+++ aceleracao_media_ms2.py
@@ -0,0 +1,7 @@
+from Acelereação import tempo_segundos, velocidade_inicial_ms
+from velocidade_final_ms import velocidade_final_ms
+# Calculando a aceleração média
+variacao_velocidade_ms = velocidade_final_ms - velocidade_inicial_ms
+
+
+aceleracao_media_ms2 = variacao_velocidade_ms / tempo_segundos
