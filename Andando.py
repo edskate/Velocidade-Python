68310047787284100f5daae652973d35c8257450
edilson gomes de lima <edskateicevida@gmail.com>
2023-08-06 17:37:14 -0300

    Veiculo velocidade de 10s

---
+++ Andando.py
@@ -0,0 +1,8 @@
+# Dados do problema
+velocidade_media_ms = 1  # velocidade média em m/s
+tempo_segundos = 120
+
+# Calculando a distância percorrida
+distancia_percorrida_m = velocidade_media_ms * tempo_segundos
+
+print("A pessoa percorre aproximadamente", distancia_percorrida_m, "metros.")
